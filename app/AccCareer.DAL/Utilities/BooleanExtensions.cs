b0b667761c20f58202871409a1e35d1edf33607e
jason-code-repo <jason.schmditlein@gmail.com>
2023-01-05 19:27:07 -0600

    Added  appsettings file

---
+++ app/AccCareer.DAL/Utilities/BooleanExtensions.cs
@@ -0,0 +1,21 @@
+﻿namespace AccCareer.DAL.Utilities;
+
+public static class BooleanExtensions
+{
+
+    public static bool ToBool(this int x)
+    {
+        return x switch
+        {
+            1 => true,
+            0 => false,
+            _ => throw new InvalidDataException("int must be either 1 or 0 to convert to bool")
+        };
+    }
+    
+    public static int ToInt(this bool x)
+    {
+        return x ? 1 : 0;
+    }
+    
+}
